6fccc1c60b3348816f5e4d911207446ecda28fe8
JanBenito <79510165+JanBenito@users.noreply.github.com>
2021-05-04 11:39:47 +0200

    Explosio regal

--- Joc Oriol, Ricard i Jan/Regal.gd
+++ Joc Oriol, Ricard i Jan/Regal.gd
@@ -8,6 +8,8 @@ var gravetat = 300
 
 func _ready():
 	velocitat = Vector2(-300,-200)
+	$Area2D/AnimatedSprite.scale = Vector2(0.08, 0.07)
+	$Area2D/AnimatedSprite.play("normal")
 
 	 
 	
@@ -16,7 +18,12 @@ func _ready():
 func _process(delta):
 	velocitat.y += gravetat * delta
 	rotation_degrees += 360 * delta
-	move_and_slide(velocitat)
+	move_and_slide(velocitat, Vector2.UP)
+	if is_on_floor():
+		$Area2D/AnimatedSprite.scale = Vector2(1,1)
+		$Area2D/AnimatedSprite.play('Explosió')
+		velocitat = Vector2.ZERO
 	
-
-
+func _on_AnimatedSprite_animation_finished():
+	if $Area2D/AnimatedSprite.animation == 'Explosió':
+		queue_free()
