3a1b7d862623228fceefa277075877fca4e8120d
JanBenito <79510165+JanBenito@users.noreply.github.com>
2021-05-10 14:07:15 +0200

    Animacio regal

--- Joc Oriol, Ricard i Jan/Regal.gd
+++ Joc Oriol, Ricard i Jan/Regal.gd
@@ -8,22 +8,34 @@ var gravetat = 300
 
 func _ready():
 	velocitat = Vector2(-300,-200)
+<<<<<<< Updated upstream
 	$Area2D/AnimatedSprite.scale = Vector2(0.08, 0.07)
+=======
+	$Area2D/AnimatedSprite.scale = Vector2(0.08,0.07)
+>>>>>>> Stashed changes
 	$Area2D/AnimatedSprite.play("normal")
 
-	 
-	
 
 
-func _process(delta):
+func _physics_process(delta):
 	velocitat.y += gravetat * delta
 	rotation_degrees += 360 * delta
 	move_and_slide(velocitat, Vector2.UP)
 	if is_on_floor():
+<<<<<<< Updated upstream
 		$Area2D/AnimatedSprite.scale = Vector2(1,1)
 		$Area2D/AnimatedSprite.play('Explosió')
 		velocitat = Vector2.ZERO
 	
 func _on_AnimatedSprite_animation_finished():
 	if $Area2D/AnimatedSprite.animation == 'Explosió':
+=======
+		velocitat = Vector2.ZERO
+		$Area2D/AnimatedSprite.scale = Vector2(1,1)
+		$Area2D/AnimatedSprite.play("Explosió")
+
+
+func _on_AnimatedSprite_animation_finished():
+	if $Area2D/AnimatedSprite.animation == "Explosió":
+>>>>>>> Stashed changes
 		queue_free()
